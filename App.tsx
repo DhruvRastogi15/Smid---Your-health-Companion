f80ef52eb764e95078eed51754c83289f2d21ad4
Dhruv Rastogi <d.rastogi15@gmail.com>
2021-12-25 15:16:49 +0530

    Screens Added

--- App.tsx
+++ App.tsx
@@ -10,70 +10,39 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from './Components/SplashScreen/SplashScreen';
+import LoginScreen from './Components/Login_SignupScreens/LoginScreen';
+import OTPScreen from './Components/Login_SignupScreens/OTPScreen';
+import HomeScreen from './Components/HomeScreens/HomeScreen';
+import DetailsScreen from './Components/HomeScreens/DetailsScreen';
 
 const Stack = createNativeStackNavigator();
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
-
 export default function App() {
+  console.disableYellowBox = true;
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="SplashScreen" component={SplashScreen} options={{
+          headerShown : false
+        }}/>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{
+          headerShown : false
+        }}/>
+        <Stack.Screen name="OTPScreen" component={OTPScreen} options={{
+          headerShown : false
+        }}/>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 // export default App;
